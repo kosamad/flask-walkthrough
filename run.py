7ca29f4c752c83ad8ab13e3ac49d70e37864d8a6
kosamad samad <KarenLSamad@gmail.com>
2024-03-15 15:47:33 +0000

    flash message with files in gitignore.

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 import os # import os from python library
 import json
-from flask import Flask, render_template, request #import flask class (request is finding out what method (in the contact form) we used 
+from flask import Flask, render_template, request, flash #import flask class (request is finding out what method (in the contact form) we used. flash is for giving feedback to user.  
+
+#import the env library only if the stystem can find an env.py file. This creates a pycache file on the left, put into gitignore
+if os.path.exists("env.py"):
+    import env
+
 
 # creat an instance of the class and store it in variable app. 
 # () = name of the applications module. We just want one so use the built in python variable __name__.
 # tells flask where to look for templates
 
 app = Flask(__name__)
+
+#grab hidden key variable
+app.sercret_key = os.environ.get("SECRET_KEY")
 
 # render_template allows python to render contents of the HTML files and render to the screen
 # in html file the format for the <a> MUST be Jinja format EXACTLY '
@@ -38,8 +46,8 @@ def about_member(member_name):
 @app.route("/contact", methods = ["GET", "POST"]) # methods tells flask we are handling a post request too (for form)
 def contact():
     if request.method == "POST":
-        print(request.form.get("name")) # this one would give us none if there wasn't a name
-        print(request.form["email"]) # this one would give an exception if there wasn't an email
+        # flash message for UX
+        flash("Thanks {}, we have received your message!".format(request.form.get("name"))) # request.form.name gets the name inputed by the user using the name attribute
     return render_template('contact.html', page_title="Contact")
 
 
